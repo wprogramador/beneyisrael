fcc0d9909715ed5267b379d438a174cf8e578b56
wprogramador <63912622+wprogramador@users.noreply.github.com>
2026-08-11 19:42:46 -0400

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -64,5 +64,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.6,
     },
+    {
+      url: `${BASE_URL}/shabbat-horarios`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
   ]
 }
